ab373804b9fa7cbfaf69c3042b96a221ad3ff94f
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-10-13 16:47:45 -0700

    Incrementing views to Parse

--- client/scripts/audition-app.js
+++ client/scripts/audition-app.js
@@ -8,18 +8,17 @@ $(document).ready(function() {
       console.log(data.results[i]);
 
       var adDisplayText = data.results[i].selector + ' ' + data.results[i].verb;
-      $('.list-ad-items').append('<li>' + linkMaker(data.results[i].urlCode,adDisplayText));
+      $('.list-ad-items').append('<li>' + linkMaker(data.results[i].objectId,adDisplayText));
     }
 
   });
 
-  $('.link-ad').on('click','a',function(e) {
+  $('.list-ad-items').on('click','a.link-ad',function(e) {
     e.preventDefault();
-
-    console.log('hello');
-    console.log($(this));
-    console.log($(this.data('special-link')));
-
+    var $element = $(this);
+    var clickedAdURLCode = $element.data('specialLink');
+    console.log(clickedAdURLCode);
+    incrementAdViewCount(clickedAdURLCode);
   });
 
   $('button.button-submit').on('click', function(e) {
@@ -109,6 +108,42 @@ var linkMaker = function(linkCode, displayText) {
   // <a href="#" class="link-ad" data-special-link="adTextValue">adDisplayText</a>
 
 };
+
+var incrementAdViewCount = function (adObjectID,callback) {
+// ad is an object with the text values and style settings for a submitted ad
+    console.log(adObjectID);
+
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/Ad/' + adObjectID,
+      type: 'PUT',
+      headers: {
+        "X-Parse-Application-Id": "pFPhY4IgGeg9xzz9T2Nlwp0uVnwY5poF1tduiBPm",
+        "X-Parse-REST-API-Key": "NXyEtXaN81pZbET54tJxyvbhZ7U4KjWat3IlESjG"
+      },
+      data: JSON.stringify({views:{__op:"Increment",amount:1}}),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('Update Made');
+
+/*         callback(data); */
+      },
+      error: function(xhr, status, errorThrown) {
+        console.log('Sorry, but we could not update the server.');
+        console.log(xhr, status, errorThrown);
+      }
+    });
+  };
+
+
+
+var linkMaker = function(linkCode, displayText) {
+  return ['<a href="#" class="link-ad" data-special-link="', linkCode, '">',displayText,'</a>'].join('');
+
+  // linkMaker should return
+  // <a href="#" class="link-ad" data-special-link="adTextValue">adDisplayText</a>
+
+};
+
 
     /*
 Application ID: pFPhY4IgGeg9xzz9T2Nlwp0uVnwY5poF1tduiBPm
